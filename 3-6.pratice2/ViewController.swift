a6fe391c787ba42b0339cbf8298de7a9c223e522
eric6742 <eric6742@gmail.com>
2022-04-13 19:08:42 +0800

    Initial Commit

---
+++ 3-6.pratice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3-6.pratice2
+//
+//  Created by eric6742 on 2022/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
